62430f113a94539547ba67065d7b9f6610a4066e
hemniels <89600300+hemniels@users.noreply.github.com>
2022-08-23 14:40:49 +0200

    initialized 'migrate_repos' and 'transformInput' files

---
+++ migrate_repos.ps1
@@ -0,0 +1,2 @@
+<# This file is used to migrate repos from one service to another.
+LogIn Credentials used in azlogin.ps1 file #>
---
+++ transformInput.py
@@ -0,0 +1,5 @@
+---
+Convert .xlsx to .csv
+To implement - input is .xlsx with pandas convert into .csv
+---
+import pandas as pd
