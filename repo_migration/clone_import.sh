c87b6208f3659c167ae70546237fa2b059ecd637
hemniels <89600300+hemniels@users.noreply.github.com>
2022-08-24 11:33:22 +0200

    cleaning+updates

--- repo_migration/clone_import.sh
+++ repo_migration/clone_import.sh
@@ -3,7 +3,7 @@ do
     az repos import create \
         --git-source-url "$URL" \
         --repository $REPO_NAME \
-        --project "NewProject" \
+        --project "PROJECT_DESCR" \
         --requires-authorization \
         --user-name 'USER_NAME'
 done
--- repo_migration/create_repos.sh
+++ repo_migration/create_repos.sh
@@ -1,4 +1,4 @@
-for REPO_NAME in "${repoArray[@]}"
+for REPO_NAME in "${REPO_ARRAY[@]}"
 do
     az repos create --name $REPO_NAME --project "PROJECT_DESCR"
 	
